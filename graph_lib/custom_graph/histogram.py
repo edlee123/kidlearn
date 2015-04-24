3cd624403172e44342e765cfd9f24608687456d4
bclement14 <benji.clement@gmail.com>
2015-04-24 15:18:22 +0200

    add histo

---
+++ graph_lib/custom_graph/histogram.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:        custom_graph
+# Purpose: 
+#
+# Authors:      Bclement, WSchueller
+#
+# Created:     14-03-2015
+# Copyright:   (c) BClement 2015
+# Licence:     CreativeCommon
+#-------------------------------------------------------------------------------
+
+from custom_graph import *
+
+class Histogram(CustomGraph):
+    def __init__(self,*arg,**kwargs):
+        CustomGraph.__init__(self,*arg,**kwargs)
